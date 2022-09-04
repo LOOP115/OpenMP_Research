67a8e22682d4cbc73b6a15773517e80db501e18a
LOOP <cjhm000115@gmail.com>
2022-09-04 16:24:08 +1000

    Compute min max flow using python

---
+++ min_max_flow.py
@@ -0,0 +1,23 @@
+import networkx as nx
+import sys
+
+
+def get_min_max_flow(input_path):
+    # Generate graph
+    graph = nx.read_edgelist(input_path, create_using=nx.DiGraph(), data=(("capacity", float),))
+    # Compute the minimum of the maximum flows between all pairs of vertices
+    min_max_flow = sys.float_info.max
+    print(f"Max Flow:")
+    for u in graph.nodes:
+        for v in graph.nodes:
+            if u == v:
+                continue
+            flow_value, flow_dict = nx.maximum_flow(graph, u, v)
+            print(f"{u} to {v}: {str(flow_value)}")
+            if flow_value > 0:
+                min_max_flow = min(min_max_flow, flow_value)
+    return min_max_flow
+
+
+path = "inputs/input1.txt"
+print(f"Min Max Flow: {get_min_max_flow(path)}")
